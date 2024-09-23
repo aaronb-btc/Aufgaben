3f240eabd4e163d8fcc40639e9c91db5f59cfd79
Bleßmann <aaron.blessmann@btc-ag.com>
2024-09-23 14:25:02 +0200

    I dont even know what i did in supermarked, but subnetting is prettier now

--- src/main/java/com/btcag/bootcamp/Subnetting.java
+++ src/main/java/com/btcag/bootcamp/Subnetting.java
@@ -5,19 +5,42 @@ import java.util.Scanner;
 public class Subnetting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Bitte gebe das erste Oktett der ip adresse an:");
-        String prefix = scanner.nextLong() + ".";
+        long okt1 = scanner.nextLong();
+
         System.out.println("Bitte gebe das zweite Oktett der ip adresse an:");
-        prefix += scanner.nextLong() + ".";
+        long okt2 = scanner.nextLong();
+
         System.out.println("Bitte gebe das dritte Oktett der ip adresse an:");
-        prefix += scanner.nextLong() + ".";
+        long okt3 = scanner.nextLong();
+
         System.out.println("Bitte gebe das vierte Oktett der ip adresse an:");
-        long ip = scanner.nextLong();
-        System.out.println("Bitte gebe die Subnetzmaske an");
+        long okt4 = scanner.nextLong();
+
+        long ip = okt1 * 1000000000 + okt2 * 1000000 + okt3 * 1000 + okt4;
+
+        System.out.println(ip + "\nBitte gebe die Subnetzmaske an");
         long mask = scanner.nextLong();
+
         long broadcast = ip | ~(-1L << (32 - mask));
-        ip -= ip & ~(-1L << (32 - mask));
-        System.out.println("Die Netzadresse ist: " + prefix + ip +
-                "\nDie Broadcastadresse ist: " + prefix + broadcast);
+        long netAddress = ip - (ip & ~(-1L << (32 - mask)));
+
+        System.out.print("Die Netzadresse ist: ");
+        String currAdress = "";
+        while (netAddress > 1000) {
+            currAdress = "." + (netAddress - ((netAddress - netAddress % 1000))) + currAdress;
+            netAddress = (netAddress - netAddress % 1000) / 1000;
+        }
+        currAdress = (netAddress - ((netAddress - netAddress % 1000))) + currAdress;
+
+        System.out.print(currAdress + "\nDie Broadcastadresse ist: ");
+        currAdress = "";
+        while (broadcast > 1000) {
+            currAdress = "." + (broadcast - ((broadcast - broadcast % 1000))) + currAdress;
+            broadcast = (broadcast - broadcast % 1000) / 1000;
+        }
+        currAdress = (broadcast - ((broadcast - broadcast % 1000))) + currAdress;
+        System.out.print(currAdress);
     }
 }
--- src/main/java/com/btcag/bootcamp/SupermarketRegister.java
+++ src/main/java/com/btcag/bootcamp/SupermarketRegister.java
@@ -7,17 +7,16 @@ public class SupermarketRegister {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte alle Produkte in Cent eingeben und mit 0 die Liste beenden:");
-        long euro = 0;
+        long cent = 0;
         long result = scanner.nextLong();
         while (result != 0) {
-            euro += result;
+            cent += result;
             result = scanner.nextLong();
         }
-        long cent = euro % 100;
-        System.out.println("Die Summe aller Produkte ist " + euro / 100 + " € und " + cent + " Cent\n\nWie viel hat der Kunde gegeben (in Cent) ?");
-        euro = scanner.nextLong() - euro;
-        cent = euro % 100;
-        euro /= 100;
+        System.out.println("Die Summe aller Produkte ist " + cent / 100 + " € und " + cent % 100 + " Cent\n\nWie viel hat der Kunde gegeben (in Cent) ?");
+        cent = scanner.nextLong() - cent;
+        long euro = cent / 100;
+        cent %= 100;
         System.out.println("Bitte folgendes Geld zurückgeben ( " + euro + "€ " + cent + " Cent)");
         long currValue = 200;
         while (currValue > 0) {
@@ -33,7 +32,7 @@ public class SupermarketRegister {
         while (currValue > 0) {
             result = cent / currValue;
             cent -= result * currValue;
-            System.out.println(result + " x " + currValue + "€");
+            System.out.println(result + " x " + currValue + " Cent");
             currValue /= 2;
             while (currValue == 25) {
                 currValue = 20;
